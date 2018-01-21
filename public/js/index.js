95106166fd2b1dd2d1915c6e9a0430bd6b7dcf90
Diego Gago <diego.gago@gmail.com>
2018-01-21 19:11:34 -0300

    Broadcasting messages

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on("connect", function() {
   console.log("connected to server");
-
-  socket.emit("createEmail", {
-    from: "me",
-    to: "jen@example.com"
-  });
 });
 
 socket.on("disconnect", function() {
--- server/server.js
+++ server/server.js
@@ -14,18 +14,17 @@ app.use(express.static(publicPath));
 
 io.on("connection", (socket) => {
   console.log("new user connected");
-
-  socket.emit("newEmail", {
-    from: "mike@example.com",
-    text: "Hey, what's going on."
-  });
-
   socket.on("createEmail", (data) => {
     console.log("create email", data);
   });
 
-  socket.on("createMessage", (data) => {
-    console.log("create message", data);
+  socket.on("createMessage", (message) => {
+    console.log("create message", message);
+    io.emit("newMessage", {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    });
   });
 
   socket.on("disconnect", () => {
